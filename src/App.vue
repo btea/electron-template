985687efa394c37234c9e96836ec9114bb5c0bae
btea <2356281422@qq.com>
2021-05-27 17:17:18 +0800

    feat: 启动

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    <h1>hello world</h1>
+    <h1>hello world！世界，你好。</h1>
   </div>
 </template>
+<script lang="ts">
+import { defineComponent, onMounted } from 'vue';
+
+export default defineComponent({
+  setup() {
+    onMounted(() => {
+      console.log(123);
+    });
+  },
+});
+</script>
