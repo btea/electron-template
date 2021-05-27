0a4e24c9891da69b5b2d5093e772e3cc1eceda08
btea <2356281422@qq.com>
2021-05-27 21:40:35 +0800

    chore: 添加node

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,14 @@
 module.exports = {
     env: {
         browser: true,
-        es2021: true
+        es2021: true,
+        node: true
     },
-    extends: ['eslint:recommended', 'plugin:vue/essential', 'plugin:@typescript-eslint/recommended'],
+    extends: [
+        'eslint:recommended',
+        'plugin:vue/essential',
+        'plugin:@typescript-eslint/recommended'
+    ],
     parserOptions: {
         ecmaVersion: 12,
         parser: '@typescript-eslint/parser',
