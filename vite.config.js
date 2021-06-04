0dc09561b2b3fd0295a30d9a4f64aa0377da5049
btea <2356281422@qq.com>
2021-06-04 16:22:14 +0800

    chore: 添加prettier配置

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import vue from '@vitejs/plugin-vue';
-export default require('vite').defineConfig({
-    base: './',
-    plugins: [vue()]
+import vue from "@vitejs/plugin-vue";
+export default require("vite").defineConfig({
+	base: "./",
+	plugins: [vue()]
 });
