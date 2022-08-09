39b0f74ebea7e5876ec8c913da93674baa7ee616
btea <2356281422@qq.com>
2022-08-09 11:30:03 +0800

    chore: format

--- start.ts
+++ start.ts
@@ -1,22 +1,22 @@
-const { createServer } = require('vite');
-const vue = require('@vitejs/plugin-vue');
-const { exec } = require('child_process');
-const port = 2233;
+const { createServer } = require('vite')
+const vue = require('@vitejs/plugin-vue')
+const { exec } = require('child_process')
+const port = 2233
 
-(async () => {
-    const server = await createServer({
-        // 任何合法的用户配置选项，加上 `mode` 和 `configFile`
-        configFile: false,
-        root: __dirname,
-        server: {
-            port
-        },
-        plugins: [vue()]
-    });
-    await server.listen();
-    exec('pnpm run start');
-})();
+;(async () => {
+  const server = await createServer({
+    // 任何合法的用户配置选项，加上 `mode` 和 `configFile`
+    configFile: false,
+    root: __dirname,
+    server: {
+      port
+    },
+    plugins: [vue()]
+  })
+  await server.listen()
+  exec('pnpm run start')
+})()
 
 module.exports = {
-    port
-};
+  port
+}
