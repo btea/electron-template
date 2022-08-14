f07767751d7e596f25d3f8a175a32a10da71ac54
btea <2356281422@qq.com>
2022-08-14 21:09:30 +0800

    chore: format code

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const url = require('url');
-const port = 2233;
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
+const port = 2233
 
-let win;
+let win
 function createWindow() {
-    win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        // backgroundColor: 'rgba(102, 204, 255, .2)',
-        icon: 'assets/icon.ico',
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
-        }
-    });
-    win.removeMenu();
-    // win.loadFile('dist/index.html');
-    win.loadURL(`http://localhost:${port}`);
-    // win.loadURL(
-    //     url.format({
-    //         pathname: 'template.html'
-    //     })
-    // );
-    // win.webContents.openDevTools();
-    win.on('closed', () => {
-        win = null;
-    });
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    // backgroundColor: 'rgba(102, 204, 255, .2)',
+    icon: 'assets/icon.ico',
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+  win.removeMenu()
+  // win.loadFile('dist/index.html');
+  win.loadURL(`http://localhost:${port}`)
+  // win.loadURL(
+  //     url.format({
+  //         pathname: 'template.html'
+  //     })
+  // );
+  // win.webContents.openDevTools();
+  win.on('closed', () => {
+    win = null
+  })
 }
 
-app.on('ready', createWindow);
+app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
 
 app.on('activate', () => {
-    if (win === null) {
-        createWindow();
-    }
-});
+  if (win === null) {
+    createWindow()
+  }
+})
--- src/App.vue
+++ src/App.vue
@@ -6,110 +6,98 @@
     <div @click="findFile" class="search">查询</div>
     <div class="menus">
       <div class="menu" @click="selectMenu(0)">{{ menus[0] }}</div>
-      <div
-        class="menu"
-        v-for="(m, i) in menus.slice(1)"
-        :key="m"
-        @click="selectMenu(i + 1)"
-      >
-        \{{ m }}
-      </div>
+      <div class="menu" v-for="(m, i) in menus.slice(1)" :key="m" @click="selectMenu(i + 1)">\{{ m }}</div>
     </div>
     <ul>
-      <li v-for="(f,i) in files" :key="i" class="item">
-        <span
-          :class="['val', { directory: f.isDirectory }]"
-          @click="findMore(f)"
-          >{{ f.name }}</span
-        >
+      <li v-for="(f, i) in files" :key="i" class="item">
+        <span :class="['val', { directory: f.isDirectory }]" @click="findMore(f)">{{ f.name }}</span>
       </li>
     </ul>
   </div>
 </template>
 <script setup lang="ts">
-import { onMounted, reactive, ref } from 'vue';
+import { onMounted, reactive, ref } from 'vue'
 
 type file = {
-  name: string,
-  isDirectory: boolean,
+  name: string
+  isDirectory: boolean
   parent: string
 }
 
 // 添加导航菜单
-const menus = reactive<string[]>([]);
+const menus = reactive<string[]>([])
 const startFind = (disk: string) => {
-  files.length = 0;
-  const directory = disk.split('\\');
-  menus.push(directory.slice(-1)[0]);
-  linkMenu(disk);
-};
-const inputDisk = ref('G');
-const files = reactive<file[]>([]);
+  files.length = 0
+  const directory = disk.split('\\')
+  menus.push(directory.slice(-1)[0])
+  linkMenu(disk)
+}
+const inputDisk = ref('G')
+const files = reactive<file[]>([])
 const findFile = () => {
-  const _disk = inputDisk.value.trim().toUpperCase();
+  const _disk = inputDisk.value.trim().toUpperCase()
   if (!_disk) {
-    return;
+    return
   }
   if (_disk < 'A' || _disk > 'Z') {
-    return;
+    return
   }
-  const disk = inputDisk.value + ':\\';
-  startFind(disk);
-  menus.length = 0;
-  menus.push(disk);
-};
+  const disk = inputDisk.value + ':\\'
+  startFind(disk)
+  menus.length = 0
+  menus.push(disk)
+}
 const findMore = (file: file) => {
   if (!file.isDirectory) {
-    return;
+    return
   }
-  const path = file.parent + '\\' + file.name;
-  startFind(path);
-};
+  const path = file.parent + '\\' + file.name
+  startFind(path)
+}
 
 const linkMenu = (disk: string) => {
-  const fs = require('fs');
-  const path = require('path');
+  const fs = require('fs')
+  const path = require('path')
   if (!fs.existsSync(disk)) {
-    console.log('该路径不存在');
-    return;
+    console.log('该路径不存在')
+    return
   }
   if (fs.lstatSync(disk).isDirectory()) {
-    let _files = fs.readdirSync(disk);
+    let _files = fs.readdirSync(disk)
     for (let f of _files) {
       try {
-        const _p = path.join(disk, f);
-        const isDirectory = fs.lstatSync(_p).isDirectory();
+        const _p = path.join(disk, f)
+        const isDirectory = fs.lstatSync(_p).isDirectory()
         const _o = {
           name: f,
           isDirectory: isDirectory,
-          parent: disk,
-        };
-        files.push(_o);
+          parent: disk
+        }
+        files.push(_o)
       } catch (error) {}
     }
   } else {
-    console.log(disk);
+    console.log(disk)
   }
-};
+}
 const selectMenu = (index: number) => {
   if (index === 0) {
-    findFile();
+    findFile()
   } else {
-    let _m = menus.slice(0, index + 1);
+    let _m = menus.slice(0, index + 1)
     if (_m.length === menus.length) {
-      return;
+      return
     }
-    menus.length = 0;
-    menus.push(..._m);
-    const disk = menus.join('\\');
-    linkMenu(disk);
+    menus.length = 0
+    menus.push(..._m)
+    const disk = menus.join('\\')
+    linkMenu(disk)
   }
-};
+}
 
 onMounted(() => {
-      console.log(123);
-});
-
+  console.log(123)
+})
 </script>
 <style lang="less" scoped>
 .disk {
